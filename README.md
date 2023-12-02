0c476ebd06ef1b2a23454f0834a965894e0e5288
kimsh322 <kimsh91322@gmail.com>
2023-12-02 22:11:48 +0900

    프로젝트 초기화

---
+++ README.md
@@ -0,0 +1,3 @@
+# React 연습용 Repository
+
+1. 팝업띄우기
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+function App() {
+  return (
+    <AppContainer>
+      <div>플레이그라운드</div>
+    </AppContainer>
+  );
+}
+
+export default App;
+
+const AppContainer = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
